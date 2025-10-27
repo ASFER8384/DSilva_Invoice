a27f5a0f53351133cbfe2a04c7ec628c643f4f8c
Asferali8384 <asferali8384@gmail.com>
2025-10-27 14:00:53 +0530

    clear

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,5 @@
 "use client";
 
-import About from "./components/About";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Homepage from "./components/Homepage";
-import Homepage2 from "./components/Homepage2";
-import Services from "./components/Services";
-
 export default function Page() {
-  return (
-    <div>
-      <Header />
-      <Homepage />
-      {/* <Homepage2 /> */}
-      <About />
-      <Services />
-      <Footer />
-    </div>
-  );
+  return <div>Market_Outlook</div>;
 }
